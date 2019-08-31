3ecb76b16d60da1caeddd517620d3556fddd347c
Ivan Gajic <igajic94@live.com>
2019-08-31 10:23:54 -0600

    finished image classification via upload functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageClassifierUploadComponent } from './image-classifier-upload/image-classifier-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageClassifierUploadComponent
   ],
   imports: [
     BrowserModule,
