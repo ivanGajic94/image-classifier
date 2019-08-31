7d920809449ccf630058065eeeba87735fbae2ab
Ivan Gajic <igajic94@live.com>
2019-08-31 10:48:40 -0600

    added some minor styling and updated output view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageClassifierUploadComponent } from './image-classifier-upload/image-classifier-upload.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ImageClassifierUploadComponent
-  ],
+  declarations: [AppComponent, ImageClassifierUploadComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
